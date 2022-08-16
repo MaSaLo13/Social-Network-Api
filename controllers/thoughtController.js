816b2eaccb08ec71fbbd62e2fcd05c2d8a5e76a0
marvinsantiago <marvinsantiago@MindYouBusiness.lan>
2022-08-16 09:39:22 -0700

    can now update thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,22 +1,33 @@
-const { User, Thought} = require('../models');
+const { User, Thought } = require('../models');
 
 const thoughtController = {
 
     // GET /api/thoughts
     getAllThoughts(req, res) {
         Thought.find({})
-        .select('-__v')
-        .then(thoughtData => res.json(thoughtData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        })
+            .select('-__v')
+            .then(thoughtData => res.json(thoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            })
     },
 
-    createThought({body}, res) {
+    createThought({ body }, res) {
         Thought.create(body)
-        .then(thoughtData => res.json(thoughtData))
-        .catch(err=> res.status(400).json(err));
+            .then(thoughtData => res.json(thoughtData))
+            .catch(err => res.status(400).json(err));
+    },
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(thoughtData => {
+            if (!thoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json(thoughtData);
+        })
+        .catch(err => res.status(400).json(err));
     },
 
 
